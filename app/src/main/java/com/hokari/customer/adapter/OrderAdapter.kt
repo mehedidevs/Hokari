25ec726cfd1274a8a78bb6c5513f6e3201fc5a32
imraj2020 <imraj5050@gmail.com>
2023-11-28 16:43:29 +0600

    Adapter fixing

--- app/src/main/java/com/hokari/customer/adapter/OrderAdapter.kt
+++ app/src/main/java/com/hokari/customer/adapter/OrderAdapter.kt
@@ -6,24 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.hokari.customer.R
+import com.hokari.customer.databinding.DashboardItemRowBinding
+import com.hokari.customer.databinding.OrderRowBinding
 import com.hokari.customer.ui.activities.OrderDetailsActivity
 
 import com.hokari.customer.model.Order
 import com.hokari.customer.utils.LoadGlide
-import kotlinx.android.synthetic.main.order_row.view.*
-
-class OrderAdapter(private val context: Context,private var list: ArrayList<Order>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    class OrderViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+class OrderAdapter(private val context: Context,private var list: ArrayList<Order>):
+    RecyclerView.Adapter<OrderAdapter.OrderViewHolder>() {
+
+    class OrderViewHolder(val  binding: OrderRowBinding) : RecyclerView.ViewHolder(binding.root)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
         return OrderViewHolder(
-        LayoutInflater.from(context).inflate(R.layout.order_row, parent, false)
+            OrderRowBinding.inflate(  LayoutInflater.from(context),
+                parent,
+                false)
         )
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
 
         val model = list[position]
 
@@ -31,12 +38,15 @@ class OrderAdapter(private val context: Context,private var list: ArrayList<Orde
 
             LoadGlide(context).loadProductImage(
                 model.image,
-                holder.itemView.iv_item_image
+                holder.binding.ivItemImage
             )
 
-            holder.itemView.tv_item_name.text = model.title
-            holder.itemView.tv_item_price.text = "$${model.total_amount}"
-            holder.itemView.ib_delete_product.visibility = View.GONE
+            holder.binding.apply {
+               tvItemName.text = model.title
+                tvItemPrice.text = "$${model.total_amount}"
+                ibDeleteProduct.visibility = View.GONE
+            }
+
 
             holder.itemView.setOnClickListener {
                 val intent = Intent(context, OrderDetailsActivity::class.java)
